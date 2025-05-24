6f0b6f47ef51562eb503c9c83346340b1b04bc25
cheapdramas <bileckijrostislav46@gmail.com>
2025-05-25 00:04:56 +0300

    - main

--- main.c
+++ main.c
@@ -1,37 +1,4 @@
-// #include "extclib/http"
-// #include <string.h>
-// void index_page(int conn, HTTPreq *request);
-//
-//
-// int main() {
-// 	HTTP *serve = new_http("127.0.0.1:7545");
-//
-// 	// handle_http(serve, path, function handler)
-// 	handle_http(serve, "/", index_page);
-//
-// 	//while loop?
-// 	listen_http(serve);
-// 	free_http(serve);
-//
-// 	return 0;
-// }
-//
-// void index_page(int conn, HTTPreq *request) {
-// 	if (strcmp(request->path,"/") != 0) {
-// 		parse_html(conn, "404.html");
-// 		return;
-// 	}
-//
-// 	parse_html(conn, "index.html");
-// }
-
 int main(){
 	return 0;
 }
 
-
-
-
-
-
-// 
